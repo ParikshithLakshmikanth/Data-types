84b1dbf4b18a00e960f2fd8d771d917a27e17b3b
ParikshithLakshmikanth <132085131+ParikshithLakshmikanth@users.noreply.github.com>
2024-09-22 17:28:07 +0530

    Data type

---
+++ main.py
@@ -0,0 +1,21 @@
+name = "ITZME"
+age = 16
+is_student = True
+weight = 40.7
+
+print("name",name)
+print("datatype of name",type(name))
+print("Age",age)
+print("datatype of age",type(age))
+print("is student",is_student)
+print("datatype of is student",type(is_student))
+print("weight",weight)
+print("datatype of weight",type(weight))
+
+print("\n after conversions")
+age = str(age)
+print("Age",age)
+print("datatype of age",type(age))
+weight = int(weight)
+print("weight",weight)
+print("datatype of weight",type(weight))
